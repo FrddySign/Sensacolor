f6e6a17dfea95d6ae06672171337889f2035421e
FrddySign <frddysign@gmail.com>
2023-03-08 02:23:04 -0300

    se agrega el proyecto

---
+++ main.js/main.js
@@ -0,0 +1,20 @@
+const $generate = document.getElementById('generate'),
+    $reset = document.getElementById('reset'),
+    $showColor = document.querySelector('.color-hex-bga-hsl');
+
+let hex = [1, 2, 3, 4, 5, 6, 7, 8, 9,"A","B","C","D","E","F"];
+
+document.addEventListener('click', e =>{
+    if (e.target === $generate){
+        let color = "#";
+        for(let i = 0; i < 6; i++){
+            color += hex[randomNumber()]
+        }
+        $showColor.innerHTML = color;
+        document.body.style.background = color;
+    }
+})
+
+function randomNumber(){
+    return Math.floor(Math.random()*hex.length)
+}
